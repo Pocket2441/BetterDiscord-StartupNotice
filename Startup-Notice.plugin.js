0141d12ef3f62732be3044788d0febe0b610cded
korben-open <81384675+korben-open@users.noreply.github.com>
2022-09-07 20:44:08 +0100

    Add files via upload

---
+++ Startup-Notice.plugin.js
@@ -0,0 +1,24 @@
+/**
+ * @name Startup Notice
+ * @author Pocket#2441
+ * @description Display A notice for when discord loads
+ * @version 0.0.1
+ */
+
+module.exports = class MyPlugin {
+  constructor(meta) {
+    // Do stuff in here before starting
+  }
+
+
+  start() {
+    // Do stuff when enabled
+ console.log("Discord is Ready to use")
+BdApi.showNotice("Discord is Ready to use");
+  }
+
+
+  stop() {
+    // Cleanup when disabled
+  }
+};
